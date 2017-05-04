bdba841123af9361ac39826f138726b0a8ee36c2
Jinjia Huang <jhuang64@id.iit.edu>
2017-05-04 02:11:03 -0500

    no message

---
+++ script.js
@@ -0,0 +1,28 @@
+$.ajax({
+    url: "http://jsonplaceholder.typicode.com/users",
+    type: "GET",
+    success: function (response) {
+        // console.log(response);
+        for (var i = 0; i < response.length; i++) {
+            var user = response[i];
+            updateUserInformation(i, user);
+        }
+    }
+});
+
+function updateUserInformation(index, user) {
+    var $user = $('.user-profile').eq(index);
+
+    $user.find('.user-name').text(user.name);
+
+    $user.find('.user-phone').text(user.phone);
+
+    $user.find('.user-company').text(user.company.name);
+
+    $user
+        .find('.user-website')
+        .find('a')
+        .text(user.website)
+        .attr('herf', "http://" + user.website);
+
+}
